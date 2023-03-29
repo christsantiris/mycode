c83015cb0dc64059addcc649220b17c65b0eb087
christsantiris <chris.tsantiris@gmail.com>
2023-03-29 18:57:02 +0000

    aws challenge

---
+++ aws-challenge/variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "name" {
+  default = "AltaResearch"
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "ami" {
+  default = "ami-830c94e3"
+}
